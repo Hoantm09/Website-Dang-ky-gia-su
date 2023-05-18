bb6373f78d564c3ed013319646d156d215adb9b6
Nguyễn Văn Huy <nvhuy0405@gmail.com>
2023-05-18 18:49:28 +0700

    giaSuquery

---
+++ www/html/src/models/giaSuModels.php
@@ -0,0 +1,23 @@
+<?php 
+class giaSuModels extends ConnectDB{
+    // Tạo dữ liệu mới
+    function CreateUser($name,$email,$password){
+        // Câu truy vấn
+        $sql = "INSERT INTO `instructor`(
+                    `name`,
+                    `email`,
+                    `password`
+                )
+                VALUES(
+                    '$name',
+                    '$email',
+                    '$password'
+                )";
+        // Thực hiện truye vấn và kiểm tra
+        mysqli_query($this->connection,$sql);
+        if (mysqli_error($this->connection) == ""){
+            return true;
+        }else{
+            return false;
+        }
+    }
