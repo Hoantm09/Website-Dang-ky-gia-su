c4a41536cc053690c08055b284dd5b6140c73f4e
Nguyễn Văn Huy <nvhuy0405@gmail.com>
2023-05-19 21:26:15 +0700

    ChangePass

--- www/html/src/models/giaSuModels.php
+++ www/html/src/models/giaSuModels.php
@@ -42,3 +42,27 @@ class giaSuModels extends ConnectDB{
             return false;
         }
     }
+    // Đổi mật khẩu
+    function ChangePass($password,$secure_pass){
+        $id = $_SESSION['id'];
+        // tìm kiếm dữ liệu qua id
+        $user = $this->selectOne('id', $id);
+        // Kiểm tra xem dữ liệu có tồn tại hay không
+        if (isset($user['password'])){
+            // Kiểm tra mật khẩu
+            $verify = password_verify($password, $user['password']);
+            if ($verify){
+                // Truy vấm cập nhập
+                $sql = "UPDATE
+                        `instructor`
+                    SET
+                        `password` = '$secure_pass'
+                    WHERE
+                        `id` = '$id'";
+                mysqli_query($this->connection,$sql);
+                return true;
+            }  
+        }else{
+            return false;
+        }
+    }
