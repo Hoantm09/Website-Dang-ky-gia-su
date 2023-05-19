3276da81e3bce3d9e0d711df7ccb5b72c7a92bd6
Nguyen Tung <nguyenquangtung2052002@gmail.com>
2023-05-19 21:55:47 +0700

    changepassUser

--- www/html/src/models/userModels.php
+++ www/html/src/models/userModels.php
@@ -43,5 +43,59 @@ class userModels extends ConnectDB{
             return false;
         }
     }
-   
+    // hàm đổi mật khẩu
+    function ChangePass($password,$secure_pass){
+        $id = $_SESSION['id'];
+        // tìm kiếm dữ liệu qua id
+        $user = $this->selectOne('id', $id);
+
+        // Kiểm tra xem dữ liệu có tồn tại hay không
+        if (isset($user['password'])){
+            // Kiểm tra mật khẩu
+            $verify = password_verify($password, $user['password']);
+            if ($verify){
+                $sql = "UPDATE
+                        `user`
+                    SET
+                        `password` = '$secure_pass'
+                    WHERE
+                        `id` = '$id'";
+                mysqli_query($this->connection,$sql);
+                return true;
+            }  
+        }else{
+            return false;
+        }
+    }
+    // Lấy dữ liệu từ 1 hàng
+    function selectOne($key,$value){
+        // Truy vấn
+        $sql = "SELECT * FROM `user` WHERE `$key` = '$value'";
+        $user = mysqli_query($this->connection,$sql);
+        // Ép dữ liệu từ mảng về 1
+        $user = mysqli_fetch_array($user);
+        return $user;
+    }
+    // Cập nhập dữ liệu
+    function updateOne($id, $name, $email, $phone_number, $gender, $avatar, $address){
+        // Câu truy vấn
+        $sql = "UPDATE
+                    `user`
+                SET
+                    `name` = '$name',
+                    `email` = '$email',
+                    `phone_number` = '$phone_number',
+                    `gender` = '$gender',
+                    `avatar` = '$avatar',
+                    `address` = '$address'
+                WHERE
+                    `id` = '$id'";
+        // Thực hiện truy vấn và kiểm tra
+        mysqli_query($this->connection,$sql);
+        if (mysqli_error($this->connection) == ""){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
